329279c243d4f2bee0ce949e55c64049ddef420e
chh4031 <chh4031@gmail.com>
2023-12-11 18:07:15 +0900

    로그인추가

--- src/router/mainRouter.js
+++ src/router/mainRouter.js
@@ -6,4 +6,13 @@ const mainRouter = require("../controller/mainController");
 // 메인 첫 화면을 보여주는 라우터
 router.get("/", mainRouter.mainView);
 
+// 로그인 화면을 보여주는 라우터, 이거 login하니깐 맛가서 logi 함 걍 하면됨
+router.get("/logi", mainRouter.login);
+
+// 로그인 체크 라우터
+router.post("/check", mainRouter.checkLogin)
+
+// 로그아웃 라우터
+router.get("/logout", mainRouter.logout)
+
 module.exports = router;
